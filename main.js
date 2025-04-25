b22670ec3de8cb4315588c711b03d129bbba8259
Mykyta web2025-5 <25nikish25@gmail.com>
2025-04-25 14:44:39 +0300

    update main.js for 3 part

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const { Command } = require('commander');
-const http = require('http');
 const fs = require('fs/promises');
+const http = require('http');
 const path = require('path');
+const superagent = require('superagent');
+const fsSync = require('fs');
 
 const program = new Command();
 
@@ -12,7 +14,7 @@ program
 
 program.parse(process.argv);
 const options = program.opts();
-const cacheDir = options.cache;
+const cacheDir = path.resolve(options.cache);
 
 function getImagePath(code) {
   return path.join(cacheDir, `${code}.jpg`);
@@ -26,54 +28,84 @@ async function directoryExists() {
   }
 }
 
-async function startServer(){
-  await directoryExists();
-  const server = http.createServer(async (req, res) => {
-    const code = req.url.slice(1);
-    const filePath = getImagePath(code);
-
-    try {
-      switch (req.method) {
-        case 'GET': {
-          try {
-            const data = await fs.readFile(filePath);
-            res.writeHead(200, {'Content-Type': 'image/jpeg'});
-            res.end(data);
-          } catch (err) {
-            if (err.code === 'ENOENT') {
-              res.writeHead(404, {'Content-Type': 'text/plain'});
-              res.end('Not found');
-            } else {
-              throw err;
-            }
-          }
-          break;
-        }
-        case 'PUT': {
-            res.writeHead(201, {'Content-Type': 'text/plain'});
-            res.end('Created');
-            break;
-        }
-        case 'DELETE': {
-          await fs.unlink(filePath);
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end('Deleted');
-          break;
-      }
-        default:
-          res.writeHead(405, {'Content-Type': 'text/plain'});
-          res.end('Method Not Allowed');
-      }
-    } catch (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.end('Internal Server Error');
+async function fileExists(path){
+    try{
+        await fs.access(path);
+        return true;
+    } catch{
+        return false;
     }
-  });
 
-  server.listen(options.port, options.host, () => {
-    console.log(`Server working on http://${options.host}:${options.port}`);
-  });
 }
+async function startServer(){
+    await directoryExists();
+    const server = http.createServer(async (req, res) => {
+        const code = req.url.slice(1);
+        const filePath = getImagePath(code);
 
+        try {
+            switch (req.method) {
+                case 'GET': {
+                    if(await fileExists(filePath)) {
+                        const data = await fs.readFile(filePath);
+                        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+                        res.end(data);
+                    }
+                    else{
+                        const url = `https://http.cat/${code}`
+                        const writeStream = fsSync.createWriteStream(filePath);
+                        superagent
+                            .get(url)
+                            .on('error', (err) => {
+                                res.writeHead(404, {'Content-Type': 'text/plain'});
+                                res.end('Not Found');
+                            })
+                            .pipe(writeStream)
+                            .on('finish', async () => {
+                                const data = await fs.readFile(filePath);
+                                res.writeHead(200, {'Content-Type': 'image/jpeg'});
+                                res.end(data);
+                            })
+                    }
+                    break;
+                }
+                case 'PUT': {
+                    const url = `https://http.cat/${code}`
+                    const writeStream = fsSync.createWriteStream(filePath);
+                    superagent
+                        .get(url)
+                        .on('error', (err) => {
+                            res.writeHead(404, {'Content-Type': 'text/plain'});
+                            res.end('Not Found');
+                        })
+                        .pipe(writeStream)
+                        .on('finish', () => {
+                        res.writeHead(201, { 'Content-Type': 'text/plain' });
+                        res.end('Created');
+    });
+                    break;
+                }
+
+                case 'DELETE': {
+                    await fs.unlink(filePath);
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    res.end('Deleted');
+                    break;
+                }
+
+                default:
+                    res.writeHead(405, {'Content-Type': 'text/plain'});
+                    res.end('Method Not Allowed');
+            }
+        } catch (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Internal Server Error');
+        }
+    });
+
+    server.listen(options.port, options.host, () => {
+        console.log(`Server working on http://${options.host}:${options.port}`);
+    })
+}
 startServer();
 
